895901dffd18412f6426768a348f9d7cf684f88b
albert-wissigkeit <wissigkeit@outlook.de>
2025-05-07 12:21:10 +0200

    step 7

--- user_auth_app/views.py
+++ user_auth_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from user_auth_app.serializers import RegistrationSerializer
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
+class  RegistrationView(CreateAPIView):
+    serializer_class = RegistrationSerializer
+    permission_classes = [AllowAny]
