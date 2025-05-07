5abe2aee4b9116ef1764c2ce65629d4768473034
albert-wissigkeit <wissigkeit@outlook.de>
2025-05-07 14:47:51 +0200

    step 12 - views

--- forum_app/views.py
+++ forum_app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from forum_app.models import Post
+from forum_app.serializers import CommentSerializer, PostSerializer
 # Create your views here.
+
+class PostListCreateView(ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class PostDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = []
+
+class CommentListCreateView(ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class CommentDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = []
