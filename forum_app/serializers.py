16777f08254869b9afb5c3c2893bee967e0b5ece
albert-wissigkeit <wissigkeit@outlook.de>
2025-05-07 14:35:34 +0200

    add serializer - step 11

---
+++ forum_app/serializers.py
@@ -0,0 +1,14 @@
+from forum_app.models import Comment, Post
+from user_auth_app import serializers
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
