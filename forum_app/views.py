7706449964bf1874975382815b8a8282d0548c01
albert-wissigkeit <wissigkeit@outlook.de>
2025-05-07 14:48:59 +0200

    fix step 12

--- forum_app/views.py
+++ forum_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from forum_app.models import Post
+from forum_app.models import Comment, Post
 from forum_app.serializers import CommentSerializer, PostSerializer
 # Create your views here.
 
@@ -10,17 +10,23 @@ class PostListCreateView(ListCreateAPIView):
     serializer_class = PostSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
 class PostDetailView(RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = []
 
 class CommentListCreateView(ListCreateAPIView):
-    queryset = Post.objects.all()
+    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
 
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
 class CommentDetailView(RetrieveUpdateDestroyAPIView):
-    queryset = Post.objects.all()
+    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = []
