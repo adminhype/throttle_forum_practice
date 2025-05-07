68ad64e4aa5b005ecd38e2f41d6a2bb9afcf3bb6
albert-wissigkeit <wissigkeit@outlook.de>
2025-05-07 15:01:40 +0200

    add core url to forum

--- core/urls.py
+++ core/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('user_auth_app.urls')),
+    path('api/', include('forum_app.urls')),
 ]
