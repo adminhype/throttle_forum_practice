b6b71f68ebceedfaadc59f1c6cdb41de1cc4aba3
albert-wissigkeit <wissigkeit@outlook.de>
2025-05-07 15:01:07 +0200

    add urls to forum - step 15

---
+++ forum_app/urls.py
@@ -0,0 +1,11 @@
+from os import path
+
+from forum_app.views import CommentDetailView, CommentListCreateView, PostDetailView, PostListCreateView
+
+
+urlpatterns = [
+    path('posts/', PostListCreateView.as_view()),
+    path('posts/<int:pk>', PostDetailView.as_view()),
+    path('comments/', CommentListCreateView.as_view()),
+    path('comments/<int:pk>', CommentDetailView.as_view()),
+]
